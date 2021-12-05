27e53386eddf7cdfd4129f6c0ae1421b609bc9c7
Youssef Elshabrawy <YIElshabrawy@gmail.com>
2021-12-05 21:17:27 +0200

    v2

--- Project/process_generator.c
+++ Project/process_generator.c
@@ -2,6 +2,7 @@
 
 void clearResources(int);
 void createClk();
+void readInputFile();
 
 int main(int argc, char * argv[])
 {
@@ -9,6 +10,7 @@ int main(int argc, char * argv[])
     // TODO Initialization
     printf("\n~ Process Generator starting ~\n");
     // 1. Read the input files.
+    readInputFile();
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
 
     // 3. Initiate and create the scheduler and clock processes.
@@ -19,12 +21,10 @@ int main(int argc, char * argv[])
         createClk();
     // 4. Use this function after creating the clock process to initialize clock
     initClk();
-    while(1){
-        sleep(1);
         // To get time use this
         int x = getClk();
         printf("current time is %d\n", x);
-    }
+
     // TODO Generation Main Loop
     // 5. Create a data structure for processes and provide it with its parameters.
     // 6. Send the information to the scheduler at the appropriate time.
@@ -37,9 +37,28 @@ void createClk() {
     execve(args[0], args, NULL);
 }
 
+void readInputFile(){
+    FILE *fptr;
+    // Handle not found
+    if ((fptr = fopen("./processes.txt", "r")) == NULL)
+    {
+        printf("\nThe file does not exist\n");
+        exit(0);
+    }
+
+    printf("\nReading the input file...\n");
+    char line[256];
+    while (fgets(line, sizeof(line), fptr))
+    {
+        if(line[0] != '#'){
+            printf("%s", line);
+        }
+    }
+}
+
 void clearResources(int signum)
 {
     //TODO Clears all resources in case of interruption
-    printf("Process Generator terminating!\n");
+    printf("\nProcess Generator terminating!\n");
     exit(0);
 }
